cac9193e641d1eb8df9ceb6dbae3043b0ac943a7
sarai gonzalez <saraiglezyglez@gmail.com>
2022-03-04 13:32:15 -0600

    googleAuth sintaxis

--- src/main.js
+++ src/main.js
@@ -2,7 +2,8 @@
 
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.6.6/firebase-app.js';
-import { getAuth, createUserWithEmailAndPassword } from 'https://www.gstatic.com/firebasejs/9.6.6/firebase-auth.js';
+import { getAuth, createUserWithEmailAndPassword, signInWithPopup, getRedirectResult, signOut } from 'https://www.gstatic.com/firebasejs/9.6.6/firebase-auth.js';
+import { GoogleAuthProvider, FacebookAuthProvider, TwitterAuthProvider } from 'https://www.gstatic.com/firebasejs/9.6.6/firebase-auth.js';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -15,7 +16,7 @@ const firebaseConfig = {
   storageBucket: "bookreads-9192a.appspot.com",
   messagingSenderId: "512279860959",
   appId: "1:512279860959:web:75245200f515c09571fb6a",
-  measurementId: "G-3327QVYEY6"
+  measurementId: "G-3327QVYEY6",
 };
 
 // Init firebase app
@@ -40,9 +41,60 @@ const createAccount = async () => {
   
   const btnSignUp = document.getElementById('btn-signUp');
   btnSignUp.addEventListener('click', createAccount);
+
+  //vincular cuenta con otros proveedores
+const googleProvider = new GoogleAuthProvider();
+const facebookProvider = new FacebookAuthProvider();
+const twitterProvider = new TwitterAuthProvider();
+
+async function signInWithPopup(auth, googleProvider) {
+    try {
+        const credential = await GoogleAuthProvider.credentialFromResult(auth);
+        console.log(userCredential.user);
+      } catch (error) {
+        console.log(error);
+      }
+}
+
+};
+  .then((result) => {
+    const credential = GoogleAuthProvider.credentialFromResult(result);
+    // The signed-in user info.
+    const user = result.user;
+    // ...
+  }).catch((error) => {
+    // Handle Errors here.
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // The email of the user's account used.
+    const email = error.email;
+    // The AuthCredential type that was used.
+    const credential = GoogleAuthProvider.credentialFromError(error);
+    // ...
+  });
+
+getRedirectResult(auth)
+  .then((result) => {
+    // This gives you a Google Access Token. You can use it to access Google APIs.
+    const credential = GoogleAuthProvider.credentialFromResult(result);
+    const token = credential.accessToken;
+
+    // The signed-in user info.
+    const user = result.user;
+  }).catch((error) => {
+    // Handle Errors here.
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // The email of the user's account used.
+    const email = error.email;
+    // The AuthCredential type that was used.
+    const credential = GoogleAuthProvider.credentialFromError(error);
+    // ...
+  });
+
+  signOut(auth).then(() => {
+    // Sign-out successful.
+  }).catch((error) => {
+    // An error happened.
+  });
   
-
-
-
-
-
