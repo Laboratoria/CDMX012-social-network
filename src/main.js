ef3daa82b6e01b26aa9bf3398d496c88f783802d
sarai gonzalez <saraiglezyglez@gmail.com>
2022-03-07 10:02:52 -0600

    sign up con fb y google

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@
 
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.6.6/firebase-app.js';
-import { getAuth, createUserWithEmailAndPassword } from 'https://www.gstatic.com/firebasejs/9.6.6/firebase-auth.js';
+import { getAuth, createUserWithEmailAndPassword, signInWithPopup, GoogleAuthProvider, FacebookAuthProvider } from 'https://www.gstatic.com/firebasejs/9.6.6/firebase-auth.js';
 import { errorArea, showSignUpError } from './ui.js';
 
 // TODO: Add SDKs for Firebase products that you want to use
@@ -66,3 +66,56 @@ const createAccount = async () => {
 
 const btnSignUp = document.getElementById('btn-signUp');
 btnSignUp.addEventListener('click', createAccount);
+
+//sign in with google
+
+const googleProvider = new GoogleAuthProvider();
+
+const btnGoogle = document.getElementById('btn-google');
+btnGoogle.addEventListener("click", () => { 
+  signInWithPopup(auth, googleProvider)
+  .then((result) => {
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    const credential = GoogleAuthProvider.credentialFromResult(result);
+    // The signed-in user info.
+    const user = result.user;
+    console.log(user);
+    // ...
+  }).catch((error) => {
+    // Handle Errors here.
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // The email of the user's account used.
+    const email = error.email;
+    // The AuthCredential type that was used.
+    const credential = GoogleAuthProvider.credentialFromError(error);
+    // ...
+  });
+})
+
+//sign in with facebook
+
+const facebookProvider = new FacebookAuthProvider();
+
+const btnFacebook = document.getElementById('btn-facebook');
+btnFacebook.addEventListener("click", () => {
+  signInWithPopup(auth, facebookProvider)
+  .then((result) => {
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    const credential = GoogleAuthProvider.credentialFromResult(result);
+    // The signed-in user info.
+    const user = result.user;
+    console.log(user);
+    // ...
+  }).catch((error) => {
+    // Handle Errors here.
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // The email of the user's account used.
+    const email = error.email;
+    // The AuthCredential type that was used.
+    const credential = FacebookAuthProvider.credentialFromError(error);
+    // ...
+  });
+})
+
