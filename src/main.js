7be81a630d6b248723aba7702c5d48e33e17480b
sonifeg <l.d.i-sfg@hotmail.com>
2022-03-09 10:05:49 -0600

    9 de marzo

--- src/main.js
+++ src/main.js
@@ -1,5 +1,42 @@
 // Este es el punto de entrada de tu aplicacion
 
-// import { myFunction } from './lib/index.js';
+// import { showAndHide } from './lib/index.js';
 
 // myFunction();
+
+// hide or show a section
+const btnJoinUs = document.getElementById('joinUs');
+const btnShootIn = document.getElementById('shootIn');
+const secLogIn = document.getElementById('logIn');
+const joinScreen = document.getElementById('joinUsScreen');
+const shootScreen = document.getElementById('shootInScreen');
+const backToaJ = document.getElementById('aJ');
+const backToaS = document.getElementById('aS');
+
+btnJoinUs.addEventListener('click', () => {
+  joinScreen.style.display = 'block';
+  if (joinScreen) {
+    secLogIn.style.display = 'none';
+  }
+});
+
+btnShootIn.addEventListener('click', () => {
+  shootScreen.style.display = 'block';
+  if (shootScreen) {
+    secLogIn.style.display = 'none';
+  }
+});
+
+backToaJ.addEventListener('click', () => {
+  secLogIn.style.display = 'block';
+  if (joinScreen) {
+    joinScreen.style.display = 'none';
+  }
+});
+
+backToaS.addEventListener('click', () => {
+  secLogIn.style.display = 'block';
+  if (shootScreen) {
+    shootScreen.style.display = 'none';
+  }
+});
