f6b16c134cbe6e5d4b992cd0e9172812b2a70c58
AnaCif98 <inv0320@innovaccion.mx>
2022-03-10 20:01:34 -0700

    SPA

---
+++ src/lib/home.js
@@ -0,0 +1,8 @@
+export const home = `
+  <div class="containerButtons">
+  <a href="#" onclick="onNavigate('/register'); return
+  false;" class="button">Registrate</a>
+  <a href="#" onclick="onNavigate('/login'); return
+  false;" class="button">Iniciar sesión</a>
+  </div>
+`;
---
+++ src/lib/register.js
@@ -0,0 +1,3 @@
+export const register = `
+<h1>ESTA ES EL REGISTRO</h1>
+`;
