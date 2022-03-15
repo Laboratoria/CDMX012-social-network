5f3455077363d91b43ad9077d789f048b9811937
Mariana <mariana.hb95@gmail.com>
2022-03-15 12:00:42 -0500

    vistas funcionales, input registro en consola

--- src/lib/login.js
+++ src/lib/login.js
@@ -1,3 +1,12 @@
 export const login = `
-<h1>ESTA ES EL LOGIN</h1>
+<button onclick="onNavigate('/'); return
+      false"><img class="arrowBack" src="img/Arrow.png"></img></button>
+<div class="containerRegister">
+  <img class="logo" src="img/Logo.png">
+  <div class="containerInputR">
+    <input type="text" class="register" placeholder="Ingresa tu correo" autocomplete="off"> 
+    <input type="text" class="register" placeholder="Ingresa tu contraseña" autocomplete="off"> 
+    <button class="registerButton">Login</button>
+  </div>
+</div>
 `;
