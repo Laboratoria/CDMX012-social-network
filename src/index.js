0e417e65afdc22d6b842559ba6058206ddf1f3a1
Atenas Villalba <atenasvillalba8@gmail.com>
2022-03-14 19:11:28 -0600

    User authentication

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import "./styles/main.css";
 
-import { addDoc, collection, getFirestore } from "firebase/firestore";
+import { getDocs, collection, getFirestore } from "firebase/firestore/lite";
 import { initializeApp } from "firebase/app";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 
-alert("connected!");
-
-const app = initializeApp({
+const firebaseConfig = {
   apiKey: "AIzaSyAwGQsOSeaKd97-2Livk2oDexObO1flAJM",
   authDomain: "solovino-d247a.firebaseapp.com",
   projectId: "solovino-d247a",
@@ -13,10 +12,41 @@ const app = initializeApp({
   messagingSenderId: "944282026707",
   appId: "1:944282026707:web:74381d8ce6df08079e0a26",
   measurementId: "G-CCE8KLPCKW",
-});
+};
 
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
+// Get a list of cities from your database
+async function getCities(db) {
+  const citiesCol = collection(db, "cities");
+  const citySnapshot = await getDocs(citiesCol);
+  const cityList = citySnapshot.docs.map((doc) => doc.data());
+  return cityList;
+}
+
+window.sendInformation = function sendInformation() {
+  let email = document.getElementById("email").value;
+  let password = document.getElementById("password").value;
+  //return alert("email=" + email + " y " + " pass= " + password);
+
+  // Registra usuarios nuevo
+  const auth = getAuth();
+  createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      // Signed in
+      const user = userCredential.user;
+      // ...
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      return alert(errorMessage);
+    });
+};
+
+/*
 try {
   const response = await addDoc(collection(db, "post"), {
     body: "Miren mi perrito, esta bonito",
@@ -27,3 +57,4 @@ try {
 } catch (error) {
   console.log(error);
 }
+*/
