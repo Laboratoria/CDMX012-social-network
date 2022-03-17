efc3a45dd4c3b519fcd52382522b484e8e833544
Sandra-Herrera <97549362+Sandra-Herrera@users.noreply.github.com>
2022-03-17 14:21:50 -0600

    funciona con firebase y nos direcciona a login

--- src/lib/register.js
+++ src/lib/register.js
@@ -1,3 +1,5 @@
+import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.6.8/firebase-auth.js"; //viene desde una CDN y no de lib
+
 export const register = `
 <a onclick="onNavigate('/'); return
       false"><img class="arrowBack" src="img/Arrow.png"></img></a>
@@ -15,6 +17,26 @@ export const register = `
     <input id="user" type="text" class="register" placeholder="Ingresa tu nombre de usuaria" autocomplete="off"> 
     <input id="password" type="text" class="register" placeholder="Ingresa tu contraseña" autocomplete="off"> 
     <input id="area" type="text" class="register" placeholder="Ingresa tu area tech" autocomplete="off"> 
-    <button id="registerButton" class="registerButton">Registrarme</button>
+    <button id="registerButton" class="registerButton" onclick="registerFireBase(event)">Registrarme</button>
   </div>
 </div>`;
+
+function registerFireBase(e){
+    e.preventDefault(); 
+    const userMail = document.getElementById('mail').value;
+    const userName = document.getElementById('user').value;
+    const userPassword = document.getElementById('password').value;
+    const userArea = document.getElementById('area').value;
+    console.log(userMail, userName, userPassword, userArea);
+
+    const auth = getAuth(); //clave para au
+    createUserWithEmailAndPassword(auth, userMail, userPassword) // Crea el usuario
+      .then((userCredential) => { //una vez creado con Éxito, devuelve las credenciales del usuario
+        const user = userCredential.user; //trae info del usuario (nos podria servir para despues)
+        console.log('¡Registro Exitoso!');
+        alert ("registrado");
+        onNavigate('/login');
+    });
+}
+
+window.registerFireBase=registerFireBase;
--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@
 import { home } from './lib/home.js';
 import { login } from './lib/login.js';
 import { register } from './lib/register.js';
-import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.6.8/firebase-auth.js"; //viene desde una CDN y no de lib
+// import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.6.8/firebase-auth.js"; //viene desde una CDN y no de lib
 
 const routes = {
   '/': home,
@@ -21,35 +21,36 @@ const onNavigate = (pathname) => {
     window.location.origin + pathname,
   );
   rootDiv.innerHTML = routes[pathname];
-  whenRoute(pathname);
+  // whenRoute(pathname);
 };
 window.onNavigate = onNavigate;
 window.onpopstate = () => {
   rootDiv.innerHTML = routes[window.location.pathname];
 };
-function whenRoute(pathname){
-  if(pathname == "/register"){
-    const registerButton = document.getElementById('registerButton');
-    registerButton.addEventListener('click', (e) => {
-      e.preventDefault();
-      const userMail = document.getElementById('mail').value;
-      const userName = document.getElementById('user').value;
-      const userPassword = document.getElementById('password').value;
-      const userArea = document.getElementById('area').value;
-      console.log(userMail, userName, userPassword, userArea);
 
-      const auth = getAuth(); //clave para au
-      createUserWithEmailAndPassword(auth, userMail, userPassword) // Crea el usuario
-        .then((userCredential) => { //una vez creado con Éxito, devuelve las credenciales del usuario
-          const user = userCredential.user; //trae info del usuario (nos podria servir para despues)
-          console.log('¡Registro Exitoso!')
-        })
+// function whenRoute(pathname){
+//   if(pathname == "/register"){
+//     const registerButton = document.getElementById('registerButton');
+//     registerButton.addEventListener('click', (e) => {
+//       e.preventDefault();
+//       const userMail = document.getElementById('mail').value;
+//       const userName = document.getElementById('user').value;
+//       const userPassword = document.getElementById('password').value;
+//       const userArea = document.getElementById('area').value;
+//       console.log(userMail, userName, userPassword, userArea);
+
+//       const auth = getAuth(); //clave para au
+//       createUserWithEmailAndPassword(auth, userMail, userPassword) // Crea el usuario
+//         .then((userCredential) => { //una vez creado con Éxito, devuelve las credenciales del usuario
+//           const user = userCredential.user; //trae info del usuario (nos podria servir para despues)
+//           console.log('¡Registro Exitoso!')
+//         })
         // .catch((error) => {
         //   const errorCode = error.code;
         //   const errorMessage = error.message;
         //   // ..
         // });
-    });
-  }
-}
-whenRoute(window.location.pathname); //se agrega whenroute para cuando actualicemos en register vuelva a tomar la funcion
+  //   });
+  // }
+// }
+// window.location.pathname; //se agrega whenroute para cuando actualicemos en register vuelva a tomar la funcion
