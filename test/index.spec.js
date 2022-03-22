3770d01e2258a64b3f2975248ee5fbc634e98d20
sarai gonzalez <saraiglezyglez@gmail.com>
2022-03-22 17:52:29 -0600

    se realizaron tres pruebas unitarias, de las cuales una falló en su mockeo de firebase porque auth y db están como variables globales

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,10 +1,47 @@
-// importamos la funcion que vamos a testear
-import { isValidField } from '../src/lib/posts';
+/**
+ * @jest-environment jsdom
+ */
+import { isValidField } from '../src/lib/posts.js';
+import { deletePost } from '../src/lib/deletePost.js';
 
 jest.mock('../src/firebase-imports.js');
 
-describe('myFunction', () => {
-  it('debería ser una función', () => {
-    expect(typeof isValidField).toBe('function');
+describe('isValidField', () => {
+  const name = '';
+  const username = '';
+  const div = document.createElement('div');
+  div.setAttribute('id', 'errorAreaForm');
+  document.body.appendChild(div);
+  it('debería ser una función que regrese false cuando los campos están vacíos', () => {
+    expect(isValidField(name, username)).toBe(false);
+  });
+  const nameTwo = 'Pedro';
+  const usernameTwo = 'Pedro';
+  it('debería ser una función que regrese true cuando los campos no están vacíos', () => {
+    expect(isValidField(nameTwo, usernameTwo)).toBe(true);
+  });
+});
+
+/* describe('usernameValidation', () => {
+  const username = 'Pedro?';
+  const div = document.createElement('div');
+  div.setAttribute('id', 'errorAreaUsername');
+  const div2 = document.createElement('div2');
+  div2.setAttribute('id', 'errorAreaForm');
+  document.body.appendChild(div, div2);
+  it('debería ser una función que regrese false cuando un usuario usa
+  caracteres especiales en su username', () => {
+    expect(usernameValidation(username)).toBe(false);
+  });
+  const usernameTwo = 'Pedro12';
+  it('debería ser una función que regrese true cuando un usuario no usa
+  caracteres especiales', () => {
+    expect(usernameValidation(usernameTwo)).toBe(true);
+  });
+}); */
+
+describe('deletePost', () => {
+  it('debería ser una función que borra post, es decir, no regresa nada', () => {
+    expect(deletePost('docRef')).toBe(undefined);
   });
 });
