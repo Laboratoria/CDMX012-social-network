1934e64c732809b41fe1ab5bc0bb7f8789abb4fb
Allison Tovar <alis.th94@gmail.com>
2022-03-24 22:32:10 -0600

    funcion maskify completa

--- src/components/home.js
+++ src/components/home.js
@@ -1,2 +1,9 @@
-/*------ AQUI VAN ELEMENTOS PARA CREAR POST------
+/* eslint-disable import/no-cycle */
+/* ------ AQUI VAN ELEMENTOS PARA CREAR POST------ */
+// import { onNavigate } from '../main.js';
 
+export const home = () => {
+  const paragraph = document.createElement('p');
+  paragraph.textContent = 'Holi, en contrucción :)';
+  return paragraph;
+};
