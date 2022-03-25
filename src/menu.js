2336770369504673325fe2a20246ce745deabc01
Sarai Rojas <sara.r.sandoval@gmail.com>
2022-03-25 16:40:49 -0600

    Sign Out function

--- src/menu.js
+++ src/menu.js
@@ -1,3 +1,7 @@
+/* eslint-disable import/no-cycle */
+import { onNavigate } from './app.js';
+import { signOutBR } from './lib/auth.js';
+
 export const menu = () => {
   const background = document.createElement('div');
   background.setAttribute('id', 'general-container');
@@ -23,7 +27,21 @@ export const menu = () => {
 
   const configuration = document.createElement('div');
   configuration.setAttribute('id', 'config-container');
-  configuration.innerHTML = '<div><img src="./assets/logout.png"></img><p>Sign Out</p></div>';
+  const SignOut = document.createElement('div');
+  const SignOutImg = document.createElement('img');
+  SignOutImg.setAttribute('src', './assets/logout.png');
+  const SignOutTxt = document.createElement('p');
+  SignOutTxt.textContent = 'Sign Out';
+  SignOut.append(SignOutImg, SignOutTxt);
+  configuration.append(SignOut);
+
+  SignOutTxt.addEventListener('click', () => {
+    signOutBR().then(() => {
+      onNavigate('/');
+    }).catch((error) => {
+      console.log(error);
+    });
+  });
 
   menuContainer.append(profileInfo, configuration);
   background.append(menuContainer);
