489494379422d4d2edeabb0370dcf8353a5c0c7b
Candy Lopez <candy.lopez.gr@gmail.com>
2022-03-25 13:54:02 -0600

    Editar y Actualizar Post

--- src/lib/firestore.js
+++ src/lib/firestore.js
@@ -3,9 +3,11 @@ import { getFirestore,
         collection,
         addDoc,
         getDocs,
+        getDoc,
         deleteDoc,
         onSnapshot,
         doc,
+        updateDoc,
     } from 'https://www.gstatic.com/firebasejs/9.6.8/firebase-firestore.js';
 import { app } from './index.js';
 
@@ -17,3 +19,5 @@ export const guardarReceta = (receta, procedimiento) => {
 export const conseguirRecetas = () => getDocs(collection(db, 'recetas'));
 export const alConseguirRecetas = (callback) => onSnapshot(collection(db, 'recetas'), callback);
 export const borrarReceta = (id) => deleteDoc(doc(db, 'recetas', id));
+export const conseguirReceta = (id) => getDoc(doc(db, 'recetas', id));
+export const actualizarReceta = (id, nuevosCampos) => updateDoc(doc(db, 'recetas', id), nuevosCampos);
--- src/mainfinal.js
+++ src/mainfinal.js
@@ -3,11 +3,15 @@ import {
  conseguirRecetas,
  alConseguirRecetas,
  borrarReceta,
+ conseguirReceta,
+ actualizarReceta,
 }
   from "./lib/firestore.js";
 
 const formPublicacion = document.getElementById('formPublicacion');
 const postPublicado = document.getElementById('postPublicado');
+let editandoReceta = false;
+let id = '';
 
 window.addEventListener('DOMContentLoaded', async () => {
   alConseguirRecetas((querySnapshot) => {
@@ -19,6 +23,7 @@ window.addEventListener('DOMContentLoaded', async () => {
         <h3>${publicacion.receta}</h3>
         <p>${publicacion.procedimiento}</p>
         <button class = 'borrarPost' data-id= "${doc.id}"> Borrar </button>
+        <button class = 'editarPost' data-id= "${doc.id}"> Editar </button>
     </div>
     `;
     });
@@ -31,6 +36,17 @@ window.addEventListener('DOMContentLoaded', async () => {
  (obtiene el id del boton desde firestore) A esto se le llama destructuración */
       });
     });
+    const editarPost = postPublicado.querySelectorAll('.editarPost');
+    editarPost.forEach((btn) => {
+      btn.addEventListener('click', async ({ target: { dataset } }) => {
+        const datosReceta = await conseguirReceta(dataset.id);
+        const recetaEditar = datosReceta.data();
+        formPublicacion.inputReceta.value = recetaEditar.receta;
+        formPublicacion.inputProcedimiento.value = recetaEditar.procedimiento;
+        editandoReceta = true;
+        id = datosReceta.id;
+      });
+    });
   });
 });
 
@@ -38,6 +54,11 @@ formPublicacion.addEventListener('submit', (e) => {
   e.preventDefault();
   const receta = formPublicacion['inputReceta'];
   const procedimiento = formPublicacion['inputProcedimiento'];
-  guardarReceta(receta.value, procedimiento.value);
+  if (!editandoReceta) {
+    guardarReceta(receta.value, procedimiento.value);
+  } else {
+    actualizarReceta(id, { receta: receta.value, procedimiento: procedimiento.value });
+  }
+
   formPublicacion.reset();
 });
