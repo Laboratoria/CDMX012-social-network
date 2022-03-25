a0332bb313ba2c557967640a2a53fcc8f3d96a25
sarai gonzalez <saraiglezyglez@gmail.com>
2022-03-24 22:43:55 -0600

    tests addInfo, deletePost, isValidField, signIn

---
+++ test/deletePost.spec.js
@@ -0,0 +1,9 @@
+import { deletePost } from '../src/lib/deletePost.js';
+
+jest.mock('../src/firebase-imports.js');
+
+describe('deletePost', () => {
+  it('debería ser una función que borra post, es decir, no regresa nada', () => {
+    expect(deletePost('docRef')).toBe(undefined);
+  });
+});
---
+++ test/signIn.spec.js
@@ -0,0 +1,13 @@
+import { signInPage } from '../src/pages/sign-in.js';
+
+jest.mock('../src/firebase-imports.js');
+
+describe('Tests of signInPage', () => {
+  it('render signInPage', () => {
+    const renderSignIn = signInPage();
+    document.body.innerHTML = '<div id="root"></div>';
+    const rootDiv = document.getElementById('root');
+    rootDiv.appendChild(renderSignIn);
+    expect(rootDiv.innerHTML).toMatchSnapshot();
+  });
+});
