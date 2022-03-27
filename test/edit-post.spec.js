8bd93da444368405c65e7a822f26eb70379235c4
Sarai Rojas <sara.r.sandoval@gmail.com>
2022-03-27 12:10:43 -0600

    Test Save EditBtn

--- test/edit-post.spec.js
+++ test/edit-post.spec.js
@@ -13,28 +13,41 @@ describe('toEditable', () => {
     date: 'Tooday',
     uid: '6fgyg87tguyg78t76t87g87t',
   };
-  const result = createPosts(postData, '6fgyg87tguyg78t76t87g87t', 'name', 'username');
-  const node = result.querySelector('.to-edit');
-  const result2 = `<form class="edit-form"><div class="post-content">
+  const elements = createPosts(postData, '6fgyg87tguyg78t76t87g87t', 'name', 'username');
+  const node = elements.querySelector('.to-edit');
+  const result = `<form class="edit-form"><div class="post-content">
   <div><img src="./assets/libro-abierto.png" class="book-icon"> <input type="text" class="reading-txt" name="reading" value="${postData.reading}"></div>
   <br>
-  <textarea name="txt" rows="5">${postData.text}</textarea>
+  <textarea name="txt" class="reading-description" rows="5">${postData.text}</textarea>
   <br>
   </div></form><input type="button" id="btn-save" value="Save">`;
-  const btn = result.querySelector('.editPost');
+  const btn = elements.querySelector('.editPost');
   btn.dispatchEvent(new Event('click'));
 
   test('Should change post content to input', () => {
-    expect(node.innerHTML).toEqual(result2);
+    expect(node.innerHTML).toEqual(result);
   });
-  test('should return from input to show content', () => {
-    const btnSave = result.querySelector('#btn-save');
-    btnSave.dispatchEvent(new Event('click'));
-    const result3 = `<div class="post-content">
-    <div><img src= "./assets/libro-abierto.png" class= "book-icon"><p><strong>  ${postData.reading}</strong></p></div> <br>
+});
+
+describe('toContent', () => {
+  document.body.innerHTML = '<div id="postsArea"></div>';
+  const postData = {
+    reading: 'Book1',
+    text: 'I like book1',
+    date: 'Tooday',
+    uid: '6fgyg87tguyg78t76t87g87t',
+  };
+  const elements = createPosts(postData, '6fgyg87tguyg78t76t87g87t', 'name', 'username');
+  const node = elements.querySelector('.to-edit');
+  const btn = elements.querySelector('.editPost');
+  btn.dispatchEvent(new Event('click'));
+  const btnSave = elements.querySelector('#btn-save');
+  btnSave.dispatchEvent(new Event('click'));
+  const result2 = `<div class="post-content">
+    <div><img src="./assets/libro-abierto.png" class="book-icon"><p><strong>  ${postData.reading}</strong></p></div> <br>
     <p>${postData.text}</p>
     </div>`;
-
-    expect(node.innerHTML).toEqual(result3);
+  test('should return from input to show content', () => {
+    expect(node.innerHTML).toEqual(result2);
   });
 });
