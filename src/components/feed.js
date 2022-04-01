26dfa4c7c03dcd67ce0a88352e95f038937b2710
Ana Cifuentes <94483016+AnaCi98@users.noreply.github.com>
2022-04-01 10:45:01 -0700

    Función de cerrar sesión
    Este contiene la función de cerrar sesión y un contenedor nuevo donde se va a inyectar el modal

--- src/components/feed.js
+++ src/components/feed.js
@@ -4,6 +4,10 @@ import { readData } from './FireStore.js';
 import { newPost } from './newPost.js';
 // eslint-disable-next-line import/no-cycle
 import { navegations } from './navegation.js';
+// eslint-disable-next-line import/no-cycle
+import { modal } from './modal.js';
+// eslint-disable-next-line import/no-cycle
+import { signOutSession } from '../lib/firebase.js';
 
 export const feed = () => {
   const feedView = document.createElement('div');
@@ -32,17 +36,32 @@ export const feed = () => {
 
   const footer = document.createElement('div');
   footer.setAttribute('id', 'feedFooter');
+  
+  // este contenedor va a recibir el componente del modal que esta en el archivo
+  // de modal.js
+  const showModal = document.createElement('div');
+  showModal.setAttribute('id', 'showModal');
 
   const navDesktop = navegations();
   navDesktop.setAttribute('id', 'navDesktop');
   const navMobile = navegations();
   navMobile.setAttribute('id', 'navMobile');
 
+
   footer.append(navMobile);
   newPostDiv.appendChild(createPost);
   header.append(logoFeed, searchFeed);
-  feedView.append(header, createPost, navDesktop, postFeed, footer);
-
+  feedView.append(header, createPost, navDesktop, postFeed, footer, showModal);
+  
+  //esta función permite que el usuario salga de sesión
+  logOutFeed.addEventListener('click', (e) => {
+    e.preventDefault();
+    while (showModal.firstChild) {
+      showModal.removeChild(modal(signOutSession));
+    }
+    showModal.appendChild(modal(signOutSession));
+  });
+  
   readData();
   return feedView;
 };
