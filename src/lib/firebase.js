bf9efe7c8583fc7b89c8f277adb4b94fed970ab6
Ana Cifuentes <94483016+AnaCi98@users.noreply.github.com>
2022-04-01 10:40:47 -0700

    Función SignOut
    Este contiene la función de cerrar sesión

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -86,6 +86,15 @@ export const loginGoogle = () => {
       // ...
     });
 };
+
+export const signOutSession = () => {
+  const auth = getAuth();
+  signOut(auth).then(() => {
+    onNavigate('/');
+  }).catch((error) => {
+    console.log(error.message);
+  });
+};
 // export const loginGoogle = () => {
 //   const auth = getAuth();
 //   const provider = new GoogleAuthProvider();
