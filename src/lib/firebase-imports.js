06dbabcddd13d20bef9e5e82c51608a9895c71b8
sonifeg <l.d.i-sfg@hotmail.com>
2022-04-06 08:33:29 -0500

    intento para renderizar los post(ya almacena los datos del post)

--- src/lib/firebase-imports.js
+++ src/lib/firebase-imports.js
@@ -7,5 +7,5 @@
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 export { initializeApp } from 'https://www.gstatic.com/firebasejs/9.6.9/firebase-app.js';
-export { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, GoogleAuthProvider, signInWithPopup, FacebookAuthProvider, signOut } from 'https://www.gstatic.com/firebasejs/9.6.9/firebase-auth.js';
-export { getFirestore, collection, addDoc, getDocs } from 'https://www.gstatic.com/firebasejs/9.6.9/firebase-firestore.js';
+export { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, GoogleAuthProvider, signInWithPopup, getRedirectResult, signInWithRedirect, FacebookAuthProvider, signOut } from 'https://www.gstatic.com/firebasejs/9.6.9/firebase-auth.js';
+export { getFirestore, collection, addDoc, getDocs, query, setDoc, getDoc, onSnapshot, deleteDoc, doc, orderBy, updateDoc, serverTimestamp } from 'https://www.gstatic.com/firebasejs/9.6.9/firebase-firestore.js';
--- src/lib/firebase-posts.js
+++ src/lib/firebase-posts.js
@@ -1,17 +1,53 @@
-/* eslint-disable import/no-unresolved */
+/* eslint-disable import/named */
 import {
-  getFirestore, collection, addDoc, getDocs, 
-} from 'firebase-imports.js';
+  collection,
+  addDoc,
+  getDocs,
+} from './firebase-imports.js';
+import { auth, db } from './firebase-config.js';
 
-export const db = getFirestore(app); // Save registerData collection in fireStore
+export const savePost = (post, date) => {
+  // const user = auth.currentUser;
+  // addDoc(collection(db, 'posts'), {
+  //   username: user.displayName,
+  //   post,
+  // });
+  const user = auth.currentUser;
+  if (user) {
+    const uid = user.uid;
+    const displayName = user.displayName;
+    const email = user.email;
+    const photo = user.photoURL;
 
-export const savePost = (username, post) => {
-  addDoc(collection(db, 'posts'), {
-    username,
-    post,
-  });
+    return addDoc(collection(db, 'posts'), {
+      post,
+      date,
+      uid,
+      displayName,
+      email,
+      photo,
+    });
+  } else {
+    // No user is signed in.
+  }
 };
 
-/* export const getPost = () => {
-    getDocs(collection(db, 'posts'));
-  }; */
+export const getPost = () => {
+  const posts = collection(db, 'posts');
+  const docPost = getDocs(posts);
+  docPost.forEach((element) => {
+    const postNews = document.getElementById('sectionContainerPost');
+    if (element.posts.exists()) {
+      const postSection = document.createElement('section');
+      postSection.className = 'sectionContainerPost';
+      const userPostValue = posts.username;
+      const postValue = posts.post;
+      const username = document.createElement('p');
+      const postText = document.createElement('p');
+      username.textContent(userPostValue);
+      postText.textContent(postValue);
+      postNews.append(username, postText);
+    }
+    return postNews;
+  });
+};
