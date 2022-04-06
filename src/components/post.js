fc992479fcbc5ad1b2975912807676c6c3a51f86
AnaCif98 <inv0320@innovaccion.mx>
2022-04-06 08:56:08 -0700

    este contiene modal para que el usuario confirme que quiere borrar publicacion

--- src/components/post.js
+++ src/components/post.js
@@ -79,8 +79,41 @@ export function renderPost(doc) {
   });
   // console.log(userVerify);
 
+  const modal = () => {
+    const containerModal = document.createElement('section');
+    containerModal.setAttribute('id', 'containerModal');
+    containerModal.setAttribute('class', 'modalContainer');
+    const containerModalB = document.createElement('section');
+    containerModalB.setAttribute('id', 'containerModalB');
+    containerModalB.setAttribute('class', 'modalContainerB');
+    const textSignOut = document.createElement('p');
+    textSignOut.setAttribute('id', 'textSignOut');
+    textSignOut.setAttribute('class', 'signOutText');
+    textSignOut.textContent = 'Si se elimina esta publicación no podrá recuperar su contenido';
+    const yesSignOut = document.createElement('p');
+    yesSignOut.setAttribute('id', 'yesSignOut');
+    yesSignOut.setAttribute('class', 'signOutYes');
+    yesSignOut.textContent = 'Eliminar';
+    const noSignOut = document.createElement('p');
+    noSignOut.setAttribute('id', 'noSignOut');
+    noSignOut.setAttribute('class', 'signOutNo');
+    noSignOut.textContent = 'Cancelar';
+    containerModalB.append(yesSignOut, noSignOut);
+    containerModal.append(textSignOut, containerModalB);
+
+    yesSignOut.addEventListener('click', (e) => {
+      e.preventDefault();
+      deletePost(idPost, userVerify);
+    });
+
+    noSignOut.addEventListener('click', () => {
+      sectionPost.removeChild(containerModal);
+    });
+    return containerModal;
+  };
+
   deletePostButton.addEventListener('click', () => {
-    deletePost(idPost, userVerify);
+    sectionPost.append(modal());
   });
   edit.addEventListener('click', () => {
     pPost.contentEditable = 'true';
@@ -99,29 +132,5 @@ export function renderPost(doc) {
   const wall = document.getElementById('postFeed');
   wall.append(sectionPost);
 
-  // delete data
-  // dots.addEventListener('click', (e) => {
-  //   e.preventDefault();
-  //   const id = e.target.parentElement.getAttribute('data-id');
-  //   deletePost(id);
-  // });
-  // dejamos el return para los tests - no cambia en el html quitarlo o ponerlo
   return sectionPost;
 }
-// export const templatePost = (doc) => {
-//   const templateHola = `
-//   <div class="templateTop">
-//     <img id="profilePicture" src="img/profilePicture.png"></img>
-//     <label class="profileName"></label>
-//     <img id="dots" src="img/dots.png"></img>
-//   </div>
-//   <p id="inputPost">${Object.values(doc.post).join(' ')}</p>
-// <div id="likeComment">
-// <p> ${Object.values(doc.date).join(' ')} </p>
-//   <img id="likePost" src="img/like.png"></img>
-//   <img id="commentPost" src="img/comment.png">${Object.values(doc.Data().comment)}</img>
-// </div>
-// `;
-//   sectionPost.innerHTML = templateHola;
-//   return sectionPost;
-// };
