87c57499a4c0ea1382fe3629260acafa927d03d1
Sandra-Herrera <97549362+Sandra-Herrera@users.noreply.github.com>
2022-04-08 15:20:05 -0500

    continuidad de test

--- test/main.spec.js
+++ test/main.spec.js
@@ -1,8 +1,11 @@
 import { onNavigate } from '../src/main.js';
 
+jest.mock('../src/lib/firebaseFunctions.js');
 describe('onNavigate', () => {
   it('Cambia a Feed', () => {
-    onNavigate('/feed');
-    expect(window.location.pathname).toEqual('/feed');
+    document.body.innerHTML = '<div id="root"></div>';
+    const rootDiv = document.getElementById('root');
+    rootDiv.appendChild(onNavigate('/feed'));
+    expect(window.location.pathname).toBe('/feed');
   });
 });
